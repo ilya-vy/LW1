2f257240e7176baa057d907b0df40acd049f2c65
ilya-vy <i_vasyutynskyi_FOAIS_19_7_b_d@knute.edu.ua>
2020-09-04 16:18:36 +0300

    fdisshit

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число X");
+            int L = Convert.ToInt32(Console.ReadLine());
+            int z = 0;
+            for (int i = 0; L != 0;  i++)
+            {
+                L = L / 10;
+            }
+            Console.WriteLine(L);
+            Console.ReadKey();
+        }
+    }
+}
